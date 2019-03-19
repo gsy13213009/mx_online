4301d3ed8be5696641f88dda82ad7e1f0f95b0d0
郭思义 <gsy@meitu.com>
2019-03-19 16:31:02 +0800

    [optimize] 定义验证码model和轮播图model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -18,3 +20,26 @@ class UserProfile(AbstractUser):
 
         def __str__(self):
             return self.username
+
+
+class EmailVerifyRecord(models.Model):
+    code = models.CharField(max_length=20, verbose_name='验证码')
+    email = models.CharField(max_length=50, verbose_name='邮箱')
+    send_type = models.CharField(max_length=10, choices=(('register', '注册'), ('forget', '找回密码')))
+    send_time = models.DateTimeField(default=datetime.now)
+
+    class Meta:
+        verbose_name = '邮箱验证码'
+        verbose_name_plural = verbose_name
+
+
+class Banner(models.Model):
+    title = models.CharField(max_length=100, verbose_name='标题')
+    image = models.ImageField(upload_to='banner/%Y/%m', verbose_name='轮播图地址')
+    url = models.URLField(max_length=200, verbose_name='访问地址')
+    index = models.IntegerField(default=100, verbose_name='顺序')
+    add_time = models.DateTimeField(default=datetime.now, verbose_name='添加时间')
+
+    class Meta:
+        verbose_name = '轮播图'
+        verbose_name_plural = verbose_name
