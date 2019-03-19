9dea9adf50c144157cbfe46686548436b8b393ed
郭思义 <gsy@meitu.com>
2019-03-19 11:17:56 +0800

    [optimize] 配置数据库为mysql

--- mx_online/settings.py
+++ mx_online/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'mx_online.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mxonline',
+        'USER': 'gsy',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1'
     }
 }
 
