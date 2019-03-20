aa04bbff54404a656d40e1c3803f009b1a8ad286
郭思义 <gsy@meitu.com>
2019-03-20 17:23:13 +0800

    [optimize] 配置static文件路径，配置url，修改index.html里面的资源目录

--- mx_online/urls.py
+++ mx_online/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path
+from django.views.generic import TemplateView
 import xadmin
 
 urlpatterns = [
     path('admin/', xadmin.site.urls),
+    path('', TemplateView.as_view(template_name='index.html'))
 ]
