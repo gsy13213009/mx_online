e0149dfe461cd4525f901f9d9321568b7539f0ea
郭思义 <gsy@meitu.com>
2019-03-20 10:55:19 +0800

    [optimize] 解决gender最大字符串5，但是女字符串是6的问题

--- apps/users/models.py
+++ apps/users/models.py
@@ -9,7 +9,7 @@ from django.db import models
 class UserProfile(AbstractUser):
     nike_name = models.CharField(max_length=50, verbose_name='昵称', default='')
     birthday = models.DateField(verbose_name='生日', null=True, blank=True)
-    gender = models.CharField(max_length=5, choices=(('male', '男'), ('female', '女')))
+    gender = models.CharField(max_length=6, choices=(('male', '男'), ('female', '女')))
     address = models.CharField(max_length=100, default='')
     mobile = models.CharField(max_length=11, null=True, blank=True)
     image = models.ImageField(upload_to='image/%Y/%m', default='image/default.png')
