eda46c9267e976ddcaf795eb1cf27f8e416e0537
郭思义 <gsy@meitu.com>
2019-03-20 14:13:27 +0800

    [optimize] 自定义过滤器已经显示的字段

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -3,6 +3,12 @@ from users.models import EmailVerifyRecord
 
 
 class EmailVerifyRecordAdmin(object):
-    pass
+    # 显示字段
+    list_display = ['code', 'email', 'send_type', 'send_time']
+    # 搜索字段
+    search_fields = ['code', 'email']
+    # 过滤器
+    list_filter = ['code', 'email', 'send_type', 'send_time']
+
 
 xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
