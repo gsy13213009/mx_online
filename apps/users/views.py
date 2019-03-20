8858ab2761913ba94c0cde5b26b5efdf87b773f1
郭思义 <gsy@meitu.com>
2019-03-20 19:08:20 +0800

    [optimize] 在setting中使用自定义的登录模块，使用邮箱方式登录

--- apps/users/views.py
+++ apps/users/views.py
@@ -1,5 +1,24 @@
 from django.contrib.auth import authenticate, login as sys_login
+from django.contrib.auth.backends import ModelBackend
+from django.db.models import Q
 from django.shortcuts import render
+
+from users.models import UserProfile
+
+
+# 自定义auth登录操作
+class CustomBackend(ModelBackend):
+
+    # 覆盖方法
+    def authenticate(self, request, username=None, password=None, **kwargs):
+        try:
+            # Q运算，求并集
+            user = UserProfile.objects.get(Q(username=username) | Q(email=username))
+            if user.check_password(password):
+                return user
+        except Exception as e:
+            return None
+
 
 # Create your views here.
 def login(request):
@@ -11,6 +30,7 @@ def login(request):
         if user is not None:
             sys_login(request, user)
             return render(request, 'index.html', {})
+        else:
+            return render(request, 'login.html', {'msg': '用户名或者密码错误'})
     elif request.method == 'GET':
         return render(request, 'login.html', {})
-    return render(request, 'login.html', {})
