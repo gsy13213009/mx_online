d66dd80338bd25971a5b673a68e9d5fb16e5aba0
郭思义 <gsy@meitu.com>
2019-03-20 16:22:28 +0800

    [optimize] 修改侧边栏的菜单名字，每个菜单默认是model的英文名字，修改users/apps.py文件

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -1,6 +1,20 @@
 import xadmin
 from users.models import EmailVerifyRecord, Banner
+from xadmin import views
 
+
+class BaseSetting(object):
+    # 设置主题为可编辑
+    enable_themes = True
+    # 设置可选择的主题样式
+    use_bootswatch = True
+
+class GlobalSettings(object):
+    # 修改后台的title
+    site_title = '慕学后台系统'
+    site_footer = '慕学在线网'
+    # 修改后台的菜单栏可收起
+    menu_style = 'accordion'
 
 class EmailVerifyRecordAdmin(object):
     # 显示字段
@@ -19,3 +33,5 @@ class BannerAdmin(object):
 
 xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
 xadmin.site.register(Banner, BannerAdmin)
+xadmin.site.register(views.BaseAdminView, BaseSetting)
+xadmin.site.register(views.CommAdminView, GlobalSettings)
