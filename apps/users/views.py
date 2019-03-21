f5d18cab4b94ce1afaaee65425d65e7dbc603531
郭思义 <gsy@meitu.com>
2019-03-21 14:56:33 +0800

    [optimize] 使用loginform表单验证数据是否合法

--- apps/users/views.py
+++ apps/users/views.py
@@ -4,6 +4,7 @@ from django.db.models import Q
 from django.shortcuts import render
 from django.views import View
 
+from users.forms import LoginForm
 from users.models import UserProfile
 
 
@@ -20,17 +21,23 @@ class CustomBackend(ModelBackend):
         except Exception as e:
             return None
 
+
 class LoginView(View):
     def get(self, request):
         return render(request, 'login.html', {})
 
     def post(self, request):
-        user_name = request.POST.get('username', '')
-        pass_word = request.POST.get('password', '')
-        # 如果用户验证成功，会返回用户对象，否则返回None
-        user = authenticate(request, username=user_name, password=pass_word)
-        if user is not None:
-            sys_login(request, user)
-            return render(request, 'index.html', {})
+        login_form = LoginForm(request.POST)
+        # 如果LoginForm校验通过
+        if login_form.is_valid():
+            user_name = request.POST.get('username', '')
+            pass_word = request.POST.get('password', '')
+            # 如果用户验证成功，会返回用户对象，否则返回None
+            user = authenticate(request, username=user_name, password=pass_word)
+            if user is not None:
+                sys_login(request, user)
+                return render(request, 'index.html', {})
+            else:
+                return render(request, 'login.html', {'msg': '用户名或者密码错误'})
         else:
-            return render(request, 'login.html', {'msg': '用户名或者密码错误'})
+            return render(request, 'login.html', {'login_form': login_form})
