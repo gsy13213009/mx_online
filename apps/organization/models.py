0badc10cf41d2e3cb3a068361e6f7b4c569e3f49
郭思义 <gsy@meitu.com>
2019-03-22 15:56:29 +0800

    [optimize] 为机构添加分类

--- apps/organization/models.py
+++ apps/organization/models.py
@@ -17,6 +17,7 @@ class CityDict(models.Model):
 class CourseOrg(models.Model):
     name = models.CharField(max_length=50, verbose_name='机构名称')
     desc = models.TextField(verbose_name='机构描述')
+    category = models.CharField(max_length=20, default='pxjg', choices=(('pxjg', '培训机构'), ('gr','个人'), ('gx', '高校')), verbose_name='机构类别')
     click_nums = models.IntegerField(default=0, verbose_name='点击数')
     fav_nums = models.IntegerField(default=0, verbose_name='收藏数')
     address = models.CharField(max_length=150, verbose_name='机构地址')
